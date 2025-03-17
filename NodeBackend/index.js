c2b9422db22c9bab43f7777cca8e040e51779a99
ayush18201 <123385183+ayush18201@users.noreply.github.com>
2025-03-17 17:21:25 +0530

    Update index.js again

--- NodeBackend/index.js
+++ NodeBackend/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000;
 
 // CORS Middleware
 app.use(cors({
-    origin: "https://ai-powered-interview-frontend.vercel.app",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
