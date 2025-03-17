f1e2710419ee1425ace0aa2f58671fe6981e2f7b
ayush18201 <123385183+ayush18201@users.noreply.github.com>
2025-03-17 17:23:00 +0530

    Update index.js again

--- NodeBackend/index.js
+++ NodeBackend/index.js
@@ -23,7 +23,7 @@ app.use(cors({
 
 // Handle Preflight Requests
 app.options('*', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "https://ai-powered-interview-frontend.vercel.app");
+    res.header("Access-Control-Allow-Origin", "https://ai-powered-interview-1-frontend.onrender.com/");
     res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.header("Access-Control-Allow-Credentials", "true");
